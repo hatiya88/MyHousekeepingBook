173cec6ce35eeb59025e13e70ba5a7330010d4a4
minesuke88 <minesuke88@gmail.com>
2021-01-05 22:45:13 +0900

    プロジェクト ファイルを追加します。

---
+++ MyHousekeepingBook/ItemForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyHousekeepingBook
+{
+    public partial class ItemForm : Form
+    {
+        public ItemForm(CategoryDataSet dsCategory)
+        {
+            InitializeComponent();
+            categoryDataSet.Merge(dsCategory);
+        }
+
+        public ItemForm(CategoryDataSet dsCategory,
+                        DateTime nowDate,
+                        string category,
+                        string item,
+                        int money,
+                        string remarks)
+        {
+            InitializeComponent();  //初期化処理
+            categoryDataSet.Merge(dsCategory);
+            monCalendar.SetDate(nowDate);
+            cmbCategory.Text = category;
+            txtItem.Text = item;
+            mtxtMoney.Text = money.ToString();
+            txtRemarks.Text = remarks;
+        }
+    }
+}
